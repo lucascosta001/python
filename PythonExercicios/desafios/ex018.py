b88f7240864c8ebe09b7f57eac457bfba6d0c48a
lucascosta001 <107582861+lucascosta001@users.noreply.github.com>
2023-10-08 16:23:38 -0300

    Update ex018.py

--- PythonExercicios/desafios/ex018.py
+++ PythonExercicios/desafios/ex018.py
@@ -1,6 +1,6 @@
-from math import cos, sin, tan
-angulo = int(input('Informe o ângulo: '))
-numsen = sin(angulo)
-numcos = cos(angulo)
-numtg = tan(angulo)
-print('O seno, cosseno e tangente do angulo {} são respectivamente {}, {} e {}'.format(angulo, numsen, numcos, numtg))
+from math import cos, sin, tan, radians
+angulorad = float(input('Informe o ângulo: '))
+angulo = radians(angulorad)
+print('O ângulo de {} tem o SENO de {:.2f}'.format(angulo, sin(angulo)))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(angulo, cos(angulo)))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(angulo, tan(angulo)))
