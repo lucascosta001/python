74a57e41ec577199ec18d1c486c85a7a4f68d305
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2025-04-01 16:12:21 -0300

    Update ex020.py

--- PythonExercicios/desafios/ex020.py
+++ PythonExercicios/desafios/ex020.py
@@ -1,4 +1,5 @@
 from random import shuffle
+print('Digite os nomes dos Alunos! ')
 aluno1 = str(input('Primeiro aluno: '))
 aluno2 = str(input('Segundo aluno: '))
 aluno3 = str(input('Terceiro aluno: '))
